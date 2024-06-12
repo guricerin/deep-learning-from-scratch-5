06e2885adb1130c7fc3ba39f45af4b2481fd2a02
shotaro-ikeguchi <shotaro-ikeguchi@sensyn-robotics.com>
2024-06-13 02:12:03 +0900

    sample_sum

--- src/step01/__init__.py
+++ src/step01/__init__.py
@@ -82,3 +82,29 @@ def sample_avg():
     sample_avg_core(2)
     sample_avg_core(4)  # 中心極限定理により正規分布に近づく
     sample_avg_core(10)
+
+
+def sample_sum_core(sample_size):
+    x_sums = []
+    N = sample_size
+    for _ in range(10000):
+        xs = []
+        for _ in range(N):
+            x = np.random.rand()
+            xs.append(x)
+        t = np.sum(xs)
+        x_sums.append(t)
+    x_norm = np.linspace(-5, 5, 1000)
+    mu = N / 2
+    sigma = np.sqrt(N / 12)
+    y_norm = common.nomal(x_norm, mu=mu, sigma=sigma)
+
+    plt.hist(x_sums, bins="auto", density=True)
+    plt.plot(x_norm, y_norm)
+    plt.title(f"N={N}")
+    plt.xlim(-1, 6)  # x軸の範囲を指定
+    plt.show()
+
+
+def sample_sum():
+    sample_sum_core(5)
